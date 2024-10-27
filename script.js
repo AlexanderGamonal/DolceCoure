fc874e77f7113fe8f3174cf63d32fbb50602318c
Alexander Gamonal <alexmg_750@hotmail.com>
2024-10-27 18:19:48 -0500

    Actualizacion modo responsive

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ setInterval(() => {
 
     document.getElementById(`slide${currentSlide}`).checked = true; // Marca el radio correspondiente
 }, 3000); // Cambia cada 3 segundos (3000 milisegundos)
+
+// script.js
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active'); // Alterna la clase 'active' para mostrar/ocultar el menú
+});
